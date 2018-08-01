660192babd67af93f278afea14278c48c11ae45d
rajyak84 <rashmikolluru@gmail.com>
2018-08-01 15:49:10 +0100

    created an object using object literal method.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-object.create
+class vehicle {
+    constructor(name,maker,engine,color){
+        this.name =name;
+        this.maker=maker;
+        this.engine=engine;
+    }
+    start(){
+        console.log("starting...");
+    }
+}
+let bike = new vehicle('harley','bullet','1340cc')
+bike.start();
